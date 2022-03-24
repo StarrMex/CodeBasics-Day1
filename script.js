a642ab5739346230fdab6074a0a80869041190e8
StarrMex <99953918+StarrMex@users.noreply.github.com>
2022-03-24 21:58:36 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var addExclamation = function (input) {
+  var newSentence = input + "!";
+  return newSentence;
+};
+
+var main = function (input) {
+  var myOutputValue = addExclamation(input);
+  return myOutputValue;
+};
