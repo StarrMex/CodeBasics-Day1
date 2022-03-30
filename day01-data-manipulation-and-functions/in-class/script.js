c32ca8c0c193228b48ec6194e8a28026051def63
StarrMex <99953918+StarrMex@users.noreply.github.com>
2022-03-30 20:49:45 +0800

    initial commit

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,35 +1,47 @@
 var fahrenheitToCelsiusMain = function (input) {
-  // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = 'hello world';
+  var fTemp = input;
+  var fToC = ((fTemp - 32) * 5) / 9;
+  var myOutputValue = fToC;
   return myOutputValue;
 };
 
 var roadTripCostBaseMain = function (input) {
+  var travelKM = input;
+  var travelMiles = travelKM * 1.6;
+  var fuelCost = (travelMiles / 9) * 2.2;
   // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = fuelCost;
   return myOutputValue;
 };
 
 var roadTripCostComfortableMain = function (input) {
+  var travelKM = input;
+  var travelMiles = travelKM * 1.6;
+  var fuelCostCar = (travelMiles / 9) * 2.2;
+  var fuelCostTrain = (travelMiles / 9) * 2;
   // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = fuelCostCar - fuelCostTrain;
   return myOutputValue;
 };
 
 var iceMachineMain = function (input) {
+  var noOfGuest = input;
+  var gramIcePerGuest = noOfGuest * 2 * 4 * 1.5;
+  var poundIcePerGuest = gramIcePerGuest * 0.0022;
+  var durationIceCube = poundIcePerGuest / 5;
   // Complete the More Comfortable: Ice Machine exercise below with iceMachineMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = durationIceCube;
   return myOutputValue;
 };
 
 var beerOrderMain = function (input) {
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var cellularDataMain = function (input) {
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
